1fb2e9aa46cf33a64198ea35b9706b477d0a3129
leowebdesigner <contato@leowebdesigner.com.br>
2024-07-22 14:14:52 -0300

    typing by user

--- src/components/services/gateway/UserGatewayHttp.ts
+++ src/components/services/gateway/UserGatewayHttp.ts
@@ -1,9 +1,10 @@
+import User from "../../../entities/user";
 import HttpAdapter from "../http/HttpClientAdapter";
 
 export default class UserGatewayHttp {
-       async login(email: string, password: string): Promise<any> {
+       async login(email: string, password: string): Promise<User> {
         const device_name = `${navigator.userAgent}`
-         await HttpAdapter.post('/auth', {
+        return await HttpAdapter.post('/auth', {
             email, 
             password,
             device_name
--- src/store/users.ts
+++ src/store/users.ts
@@ -1,18 +1,20 @@
 import { defineStore } from "pinia"
 import UserGatewayHttp from "../components/services/gateway/UserGatewayHttp"
+import User from "../entities/user"
 
 const UserGateway = new UserGatewayHttp
 
 export const useUsersStore = defineStore('users', {
     state: () => ({
-        users: [] 
+        me: null as null | User,
+        users: [] as User[]
     }),
     getters: {
         hasUsers: (state) => state.users.length > 0 
     },
     actions: {
-        auth(email: string, password: string): Promise<any> {
-            return UserGateway.login(email, password)
+        async auth(email: string, password: string): Promise<any> {
+            return await UserGateway.login(email, password)
         }
     }
 })
